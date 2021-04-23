8d63eab8edee54ef7f042463744598c1ad799be0
David Letaw <jdletaw@gmail.com>
2021-04-22 23:19:47 -0700

    adds Folder and File components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import React from 'react';
+
 function App() {
-  return 'hello world'
+  return <div>
+    <Folder name="Desktop">
+      Some stuff
+    </Folder>
+    <Folder name="Applications"/>
+    <File name ="Dogs.jpg"/>
+  </div>
+}
+
+const Folder = (props) => {
+  console.log(props)
+  return <h3>{props.name}</h3>
+};
+
+const File = (props) => {
+  return <div>{props.name}</div>
 }
 
 export default App;
